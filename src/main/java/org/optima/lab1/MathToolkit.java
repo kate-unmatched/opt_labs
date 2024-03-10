d3e7d84095f94e40e321b65c3442c1eb48334e5d
Kate_unmatched <KatyushenkaMarchenko@mail.ru>
2024-03-11 03:19:11 +0400

    Implementation of the golden ratio method without tests

--- src/main/java/org/optima/lab1/MathToolkit.java
+++ src/main/java/org/optima/lab1/MathToolkit.java
@@ -1,10 +1,12 @@
 package org.optima.lab1;
 
-import org.optima.utils.DefaultNum;
 import org.optima.utils.NumCharacteristics;
 import org.optima.utils.UsualMethods;
+import static java.lang.Math.*;
 
 import java.util.function.DoubleUnaryOperator;
+
+import static org.optima.utils.DefaultNum.REVERSE_GRP;
 
 public class MathToolkit {
     public static double dichotomy(DoubleUnaryOperator function, double x1, double x2, double eps, int maxIterations) {
@@ -22,14 +24,14 @@ public class MathToolkit {
 
             root = (x1 + x2) * .5;
 
-            double fRootLeft = function.applyAsDouble(root-eps);
-            double fRootRight = function.applyAsDouble(root+eps);
+            double fRootLeft = function.applyAsDouble(root - eps);
+            double fRootRight = function.applyAsDouble(root + eps);
 
-            if (fRootRight > fRootLeft) {
+            if (fRootRight > fRootLeft)
                 x2 = root;
-            } else {
+            else
                 x1 = root;
-            }
+
             iteration++;
         }
         return root;
@@ -45,8 +47,85 @@ public class MathToolkit {
         return UsualMethods.customParam(function, x1, x2, levelEps, levelIter, MathToolkit::dichotomy);
     }
 
+    public static double goldenRatioMinimum(DoubleUnaryOperator function, double x1, double x2, double eps, int maxIterations) {
+
+        if (x1 > x2) {
+            double tmp = x1;
+            x1 = x2;
+            x2 = tmp;
+        }
+
+        int iteration = 0;
+
+        while (iteration < maxIterations && abs(x2 - x1) > eps){
+
+            double leftBoundary = x2 - (x2 - x1) * REVERSE_GRP;
+            double rightBoundary = x1 + (x2 - x1) * REVERSE_GRP;
+
+            double fRootLeft = function.applyAsDouble(leftBoundary);
+            double fRootRight = function.applyAsDouble(rightBoundary);
+
+            if (fRootLeft >= fRootRight)
+                x1 = leftBoundary;
+            else
+                x2 = rightBoundary;
+
+            iteration++;
+        }
+
+        return (x1 + x2) * .5;
+    }
+
+    public static double goldenRatioMinimum(DoubleUnaryOperator function, double x1, double x2,
+                                   double eps, NumCharacteristics levelIter) {
+        return UsualMethods.customParam(function, x1, x2, eps, levelIter, MathToolkit::goldenRatioMinimum);
+    }
+
+    public static double goldenRatioMinimum(DoubleUnaryOperator function, double x1, double x2,
+                                   NumCharacteristics levelEps, NumCharacteristics levelIter) {
+        return UsualMethods.customParam(function, x1, x2, levelEps, levelIter, MathToolkit::goldenRatioMinimum);
+    }
+
+    public static double goldenRatioMaximum(DoubleUnaryOperator function, double x1, double x2, double eps, int maxIterations) {
+
+        if (x1 > x2) {
+            double tmp = x1;
+            x1 = x2;
+            x2 = tmp;
+        }
+
+        int iteration = 0;
+
+        while (iteration < maxIterations && abs(x2 - x1) > eps){
+
+            double leftBoundary = x2 - (x2 - x1) * REVERSE_GRP;
+            double rightBoundary = x1 + (x2 - x1) * REVERSE_GRP;
+
+            double fRootLeft = function.applyAsDouble(leftBoundary);
+            double fRootRight = function.applyAsDouble(rightBoundary);
+
+            if (fRootLeft <= fRootRight)
+                x1 = leftBoundary;
+            else
+                x2 = rightBoundary;
+
+            iteration++;
+        }
+
+        return (x1 + x2) * .5;
+    }
+
+    public static double goldenRatioMaximum(DoubleUnaryOperator function, double x1, double x2,
+                                            double eps, NumCharacteristics levelIter) {
+        return UsualMethods.customParam(function, x1, x2, eps, levelIter, MathToolkit::goldenRatioMaximum);
+    }
+
+    public static double goldenRatioMaximum(DoubleUnaryOperator function, double x1, double x2,
+                                            NumCharacteristics levelEps, NumCharacteristics levelIter) {
+        return UsualMethods.customParam(function, x1, x2, levelEps, levelIter, MathToolkit::goldenRatioMaximum);
+    }
 
     public static void main(String[] args) {
-        System.out.println(dichotomy(x -> x * x - 4, 1, 3, 0.0001, 100));
+        System.out.println(goldenRatioMinimum(x -> x * x - 4, 1, 3, 0.0001, 100));
     }
 }
--- src/main/java/org/optima/utils/DefaultNum.java
+++ src/main/java/org/optima/utils/DefaultNum.java
@@ -1,5 +1,7 @@
 package org.optima.utils;
 
+import static java.lang.Math.*;
+
 public class DefaultNum {
     public static final double EPSILON_LOW    = 1e-3;
     public static final double EPSILON_MIDDLE = 1e-6;
@@ -8,6 +10,9 @@ public class DefaultNum {
     public static final int MAX_ITERATIONS_LOW = 100;
     public static final int MAX_ITERATIONS_MIDDLE = 500;
     public static final int MAX_ITERATIONS_HIGH = 1000;
+
+    public static final double GOLDEN_RATIO_PROPORTION = (1 + sqrt(5))/2;
+    public static final double REVERSE_GRP = 1. / GOLDEN_RATIO_PROPORTION;
 
 
 }
